0e952d64086225a6a6a1d0cb31667cf80d258c6a
Alexander Bespalov <discrimy.off@gmail.com>
2024-11-25 12:49:32 +0300

    Add save to postgres

--- bookvoed/pipelines.py
+++ bookvoed/pipelines.py
@@ -6,8 +6,36 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
-
-
+import psycopg
+import os
+import time
 class BookvoedPipeline:
+    def __init__(self):
+        self.connection = psycopg.connect(os.getenv('CONNECTION_STRING'))
+        cursor = self.connection.cursor()
+        cursor.execute('select 1')
+        test_result = cursor.fetchall()
+        if test_result != [(1,)]:
+            raise Exception('Error while initilizing DB connection')
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS items (
+                id serial PRIMARY KEY,
+                name VARCHAR(265) NOT NULL,
+                author VARCHAR(256),
+                price VARCHAR(256)             
+            )
+        """)
+        self.connection.commit()
+
     def process_item(self, item, spider):
+        time.sleep(1)
+        if 'error' in item:
+            print(f'Error item recieved, skip: {item}')
+            return item
+        
+        cursor = self.connection.cursor()
+        cursor.execute('INSERT INTO items (name, author, price) VALUES (%s, %s, %s)', [item['name'], item['author'], item['price']])
+        self.connection.commit()
+
         return item
